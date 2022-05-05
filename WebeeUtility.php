1e5a8e5ed0fd19c594c1c47056eaa6134ec5f7c8
Sabir7565 <mohdsabir7565@gmail.com>
2022-05-05 19:24:23 +0530

    changes for naming convention

--- WebeeUtility.php
+++ WebeeUtility.php
@@ -1,17 +1,17 @@
 <?php
 
 /*
-Plugin Name: WebeeUtility
+Plugin Name: BS Crousel
 Plugin URI: https://www.webeesocial.com
-Description: WebeeUtility is an elementor addon which is create using method of custom plugin.
+Description: BS-Crousel is an elementor addon which is create using method of custom plugin.
 Author: SabirWebeeSocial
 Author URI: https://www.webeesocial.com
 */
 
 function register_webee_utility_widgets($widgets_manager){
-    require_once(__DIR__."/Widgets/Crousel/CrouselWidget.php");
+    require_once(__DIR__."/Widgets/Carousel/CarouselWidget.php");
 
-    $widgets_manager->register(new CrouselWidget);
+    $widgets_manager->register(new CarouselWidget);
 }
 
 add_action('elementor/widgets/register', 'register_webee_utility_widgets');
